17e7ce34161d2ac56d56fe5a13fd83bd1e6e78c8
gh0st <75190364+gh0st68@users.noreply.github.com>
2026-04-05 00:32:21 -0400

    Remove Electron source — distribute binaries only

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  isElectron: true,
-  showNotification: (title, body) => ipcRenderer.send('show-notification', title, body),
-});
